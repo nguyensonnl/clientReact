3abdf642b8e84b10bd742b80ebeb4656d0103d7f
=nguyenson98 <nguyensonkhv26598@gmail.com>
2022-07-19 17:12:09 +0700

    Header admin

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -24,7 +24,8 @@ export default (history) => combineReducers({
     router: connectRouter(history),
     //admin: persistReducer(adminPersistConfig, adminReducer),
     user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer
+    // app: appReducer
+    app: persistReducer(appPersistConfig, appReducer)
 })
 
 const userPersistConfig = {
@@ -32,3 +33,9 @@ const userPersistConfig = {
     key: 'user',
     whitelist: ['isLoggedIn', 'userInfo']
 };
+
+const appPersistConfig = {
+    ...persistCommonConfig,
+    key: 'app',
+    whitelist: ['language']
+}
